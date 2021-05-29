085f8fe0f3783934ca8edad1fdc544b59c0afda5
Daniela <daniela.leloutre@gmail.com>
2021-05-29 13:47:58 -0300

    agrego el parser de requests de arrays y tests

--- src/services/protocol_parser_service.rs
+++ src/services/protocol_parser_service.rs
@@ -1,6 +1,5 @@
 use std::error::Error;
 use std::fmt;
-use std::convert::TryInto;
 use crate::entities::error::ParseError;
 use crate::entities::datatype_trait::DataType;
 use crate::entities::resp_types::RESPTypes;
@@ -19,9 +18,6 @@ impl fmt::Display for ParseError {
         write!(f, "ParseError occurred")
     }
 }
-
-
-
 
 impl DataType for RESPTypes {
     fn deserialize(self) -> String {
@@ -51,7 +47,6 @@ impl DataType for RESPTypes {
         }
     }
 }
-
 
 fn search_crlf(req: &[u8]) -> Result<usize, ParseError> {
     let mut i = 0;
@@ -97,18 +92,62 @@ pub fn parse(request: &[u8]) -> Result<RESPTypes, ParseError> {
                 Ok(i) => {
                     let size = read_int(pos+1, i, request);
                     pos += i+1; //salto el \r\n
-                    let request_stringified = String::from_utf8_lossy(&request[pos..]).to_string();
-                    let vec: Vec<&str> = request_stringified.split("$").collect();
                     let mut vec2: Vec<RESPTypes> = Vec::new();
-                    println!("vec stringified: {:?}", vec);
-                    for string in vec {
-                        let respie = parse(string.as_bytes());
-                        match respie {
-                            Ok(resp) => { vec2.push(resp) }
-                            Err(e) => { return Err(e); }
+                    let mut contents = &request[pos+1..];
+                    let mut idx = 0;
+                    while contents.len() > 0 {
+                        if contents[idx] == b'$' {
+                            //leer hasta el segundo crlf
+                            let first_crlf = search_crlf(contents);
+                            match first_crlf {
+                                Ok(final_pos_1) => {
+                                    // final_pos_1 deberia ser == 2
+                                    //busco final de segundo crlf
+                                    let second_crlf = search_crlf(&contents[final_pos_1+2..]);
+                                    match second_crlf {
+                                        Ok(final_pos_2) => {
+                                            let bulkstr = parse(&contents[..final_pos_1+final_pos_2+4]);
+                                            match bulkstr {
+                                                Ok(result) => {
+                                                    vec2.push(result);
+                                                    contents = &contents[final_pos_1+final_pos_2+4..];
+                                                }
+                                                Err(e) => {println!("error1");break;}
+                                            }
+                                        }
+                                        Err(e) => {println!("error2");break;}
+                                    }
+                                }
+                                Err(e) => {println!("error3");break;}
+                            }
+                        } else {
+                            //leer hasta el primer crlf
+                            println!("NO soy un bulkstring");
+                            //leer hasta el segundo crlf
+                            let first_crlf = search_crlf(contents);
+                            match first_crlf {
+                                Ok(final_pos_1) => {
+                                    // final_pos_1 deberia ser == 2
+                                    //busco final de segundo crlf
+                                    println!("first crlf:{}", final_pos_1);
+                                    let resp = parse(&contents[..final_pos_1+2]);
+                                        match resp {
+                                            Ok(result) => {
+                                                println!("result: {:?}", result);
+                                                vec2.push(result);
+                                                contents = &contents[final_pos_1+2..];
+                                                println!("contents after:{:?}", contents);
+                                            }
+                                            Err(e) => {println!("error4");break;}
+                                        }
+                                }
+                                Err(e) => {println!("error5");break;}
+                            }
                         }
                     }
-                    //validar size con vec2.len()
+                    if vec2.len() != size {
+                        return Err(ParseError::InvalidProtocol(String::from("Invalid array size")));
+                    }
                     Ok(RESPTypes::RArray(vec2))
                 }
                 Err(e) => Err(e)
@@ -181,7 +220,7 @@ pub fn parse(request: &[u8]) -> Result<RESPTypes, ParseError> {
                             let s = read_word(0, pp, slice);
                             println!("string final {}", s);
                             // validate s.len == i
-                            if s.len() != i {
+                            if s.len() != int {
                                 //ERROR
                                 return Err(ParseError::InvalidProtocol("bad2".to_string()))
                             }
@@ -199,85 +238,6 @@ pub fn parse(request: &[u8]) -> Result<RESPTypes, ParseError> {
         }
     }
 }
-
-// Clients send commands to a Redis server as a RESP Array of Bulk Strings
-pub fn parse_request(request: &[u8]) -> Result<Vec<String>, ParseError> {
-    // VALIDAR BULK STRINGS 512MB
-    let mut pos = 0;
-    let request_stringified = String::from_utf8_lossy(request).to_string();
-    let mut request_vec = Vec::new();
-    for letter in request_stringified.chars() {
-        request_vec.push(letter);
-    }
-
-    //chequeo que sea un array of bulk strings
-    if request_vec[pos] != '*' {
-        println!("Error 1");
-        return Err(ParseError::InvalidProtocol("Request must start with *".to_string()));
-    }
-
-    //chequeo que siempre que haya un /r le siga un /n
-    let mut found = false;
-    for (count, b) in request.iter().enumerate() {
-        if b == &b'\r' {
-            found = true;
-            if request[count+1] != b'\n' {
-                println!("Error 2");
-                return Err(ParseError::InvalidProtocol("Invalid CRLF, '\r' must be followed by '\n'".to_string()));
-            }
-        }
-    }
-
-    if !found {
-        println!("Error 3");
-        return Err(ParseError::InvalidProtocol("CRLF missing".to_string()));
-    }
-
-    pos += 1;
-    let mut parsed_command: Vec<String> = Vec::new();
-    let array_size = request_vec[pos].to_digit(10).unwrap();
-    let mut splitted_request: Vec<&[u8]> = request.split(|b| b == &b'$').collect();
-
-    if splitted_request.len() > 1 {
-        splitted_request.remove(0);
-    } else {
-        //revisar este
-        println!("Error 4");
-        return Err(ParseError::InvalidProtocol("Invalid number of elements".to_string()));
-    }
-    // valido array size
-    if splitted_request.len() != array_size.try_into().unwrap() {
-        println!("Error 5");
-        return Err(ParseError::InvalidProtocol("Invalid number of elements".to_string()));
-    }
-    // recorro array para ir decodificando cada elemento
-    while (pos+4) < request_vec.len() {
-        //valido que haya crlf
-        if request[pos+1] != b'\r' {
-            println!("Error 6");
-            return Err(ParseError::InvalidProtocol("CRLF missing".to_string()));
-        }
-        pos += 4; // /r/n
-        let string_len: usize = request_vec[pos].to_digit(10).unwrap().try_into().unwrap();
-        if request[pos+1] != b'\r' {
-            println!("Error 6");
-            return Err(ParseError::InvalidProtocol("CRLF missing".to_string()));
-        }
-        pos += 3; // /r/n
-        let instruction = &request[pos..pos+string_len];
-        let command: String = String::from_utf8_lossy(instruction).to_string().to_lowercase();
-        parsed_command.push(command);
-        pos += string_len-1;
-    }
-    if request_vec[request_vec.len()-2] != '\r' {
-        println!("Error 7");
-        return Err(ParseError::InvalidProtocol("CRLF missing".to_string()));
-    }
-    println!("command final: {:?}", parsed_command);
-    Ok(parsed_command)
-}
-
-
 
 pub fn parse_response(response: RESPTypes) -> String {
     return response.deserialize();
@@ -339,13 +299,55 @@ fn parse_request_returns_bulkstring_ok() {
 
 #[test]
 fn parse_request_returns_array_of_bulkstrings_ok() {
-    let req = b"*2\r\n$6\r\nfoobar\r\n$3\r\nkey\r\n";
+    let req = b"*3\r\n$6\r\nfoobar\r\n$3\r\nkey\r\n$5\r\nvalue\r\n";
     let result = parse(req);
     println!("{:?}", result);
     assert!(result.is_ok());
     match result.unwrap() {
         RESPTypes::RArray(v) => {
-            assert_eq!(v, vec![RESPTypes::RBulkString(String::from("foobar")), RESPTypes::RBulkString(String::from("key"))])
+            assert_eq!(v, vec![RESPTypes::RBulkString(String::from("foobar")), RESPTypes::RBulkString(String::from("key")), RESPTypes::RBulkString(String::from("value"))])
+        }
+        _ => assert!(false)
+    }
+}
+
+#[test]
+fn parse_request_returns_array_of_bulkstrings_and_integers_ok() {
+    let req = b"*4\r\n$6\r\nfoobar\r\n:5\r\n:10\r\n$5\r\nvalue\r\n";
+    let result = parse(req);
+    println!("{:?}", result);
+    assert!(result.is_ok());
+    match result.unwrap() {
+        RESPTypes::RArray(v) => {
+            assert_eq!(v, vec![RESPTypes::RBulkString(String::from("foobar")), RESPTypes::RInteger(5), RESPTypes::RInteger(10), RESPTypes::RBulkString(String::from("value"))])
+        }
+        _ => assert!(false)
+    }
+}
+
+#[test]
+fn parse_request_returns_array_of_errors_ok() {
+    let req = b"*2\r\n-ErrorMessage1\r\n- SomeError Message2\r\n";
+    let result = parse(req);
+    println!("{:?}", result);
+    assert!(result.is_ok());
+    match result.unwrap() {
+        RESPTypes::RArray(v) => {
+            assert_eq!(v, vec![RESPTypes::RError(String::from("ErrorMessage1")), RESPTypes::RError(String::from(" SomeError Message2"))])
+        }
+        _ => assert!(false)
+    }
+}
+
+#[test]
+fn parse_request_returns_error_when_invalid_array_size() {
+    let req = b"*4\r\n-ErrorMessage1\r\n- SomeError Message2\r\n";
+    let result = parse(req);
+    println!("{:?}", result);
+    assert!(result.is_err());
+    match result.unwrap_err() {
+        ParseError::InvalidProtocol(s) => {
+            assert_eq!(s, "Invalid array size".to_string())
         }
         _ => assert!(false)
     }
@@ -438,119 +440,6 @@ fn parse_response_vector_of_integers_ok() {
 #[test]
 fn parse_response_vector_of_errors_ok() {
     let result = parse_response(RESPTypes::RArray(vec![RESPTypes::RError("message1".to_string()), RESPTypes::RError("message2".to_string())]));
-    let expected = "*2\r\n-Error message1\r\n-Error message2\r\n".to_string();
+    let expected = "*2\r\n-message1\r\n-message2\r\n".to_string();
     assert_eq!(result, expected);
 }
-
-#[test]
-fn parse_returns_error_when_invalid_command_type() {
-    let command = b"+3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n";
-    let result = parse_request(command);
-    assert!(result.is_err());
-    match result.unwrap_err() {
-        ParseError::InvalidProtocol(_) => {}
-        _ => assert!(false)
-    }
-}
-
-#[test]
-fn parse_returns_error_when_wrong_number_of_elements() {
-    let command = b"*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n";
-    let result = parse_request(command);
-    assert!(result.is_err());
-    match result.unwrap_err() {
-        ParseError::InvalidProtocol(_) => {}
-        _ => assert!(false)
-    }
-}
-
-#[test]
-fn parse_returns_error_when_array_of_invalid_type() {
-    let command = b"*3\r\n:3\r\nSET\r\n:3\r\nkey\r\n:5\r\nvalue\r\n";
-    let result = parse_request(command);
-    assert!(result.is_err());
-    match result.unwrap_err() {
-        ParseError::InvalidProtocol(_) => {}
-        _ => assert!(false)
-    }
-}
-
-#[test]
-fn parse_returns_error_when_array_with_some_invalid_type() {
-    let command = b"*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n:5\r\nvalue\r\n";
-    let result = parse_request(command);
-    assert!(result.is_err());
-    match result.unwrap_err() {
-        ParseError::InvalidProtocol(_) => {}
-        _ => assert!(false)
-    }
-}
-
-#[test]
-fn parse_returns_error_when_invalid_length_of_string() {
-    let command = b"*3\r\n$3\r\nSET\r\n$5\r\nkey\r\n$5\r\nvalue\r\n";
-    let result = parse_request(command);
-    assert!(result.is_err());
-    match result.unwrap_err() {
-        ParseError::InvalidProtocol(_) => {}
-        _ => assert!(false)
-    }
-}
-
-#[test]
-fn parse_returns_error_when_invalid_crlf() {
-    let command = b"*3\r\n$3\r\nSET\r\n$3\rkey\r\n$5\r\nvalue\r\n";
-    let result = parse_request(command);
-    assert!(result.is_err());
-    match result.unwrap_err() {
-        ParseError::InvalidProtocol(_) => {}
-        _ => assert!(false)
-    }
-}
-
-#[test]
-fn parse_returns_error_when_missing_crlf() {
-    let command = b"*3\r\n$3\r\nSET$3\r\nkey\r\n$5\r\nvalue\r\n";
-    let result = parse_request(command);
-    assert!(result.is_err());
-    match result.unwrap_err() {
-        ParseError::InvalidProtocol(_) => {}
-        _ => assert!(false)
-    }
-}
-
-#[test]
-fn parse_returns_error_when_missing_last_crlf() {
-    let command = b"*3\r\n$3\r\nSET$3\r\nkey\r\n$5\r\nvalue";
-    let result = parse_request(command);
-    assert!(result.is_err());
-    match result.unwrap_err() {
-        ParseError::InvalidProtocol(_) => {}
-        _ => assert!(false)
-    }
-}
-
-#[test]
-fn parse_returns_ok_when_valid_set_command() {
-    let command = b"*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n";
-    let result = parse_request(command);
-    assert!(result.is_ok());
-    assert_eq!(result.unwrap(), vec!["set".to_string(), "key".to_string(), "value".to_string()]);
-}
-
-#[test]
-fn parse_returns_ok_when_valid_info_command() {
-    let command = b"*1\r\n$4\r\nINFO\r\n";
-    let result = parse_request(command);
-    assert!(result.is_ok());
-    assert_eq!(result.unwrap(), vec!["info".to_string()]);
-}
-
-#[test]
-fn parse_returns_ok_when_valid_info_with_parameters_command() {
-    let command = b"*2\r\n$4\r\nINFO\r\n$6\r\nserver\r\n";
-    let result = parse_request(command);
-    assert!(result.is_ok());
-    assert_eq!(result.unwrap(), vec!["info".to_string(), "server".to_string()]);
-}
-
