89ee8ec40bf37306b20b6b4d1f1f3ad35ae1c949
Sol Orive <sol.orive@mercadolibre.com>
2021-06-06 23:17:36 -0300

    Saco los test que no cumplen con las especificaciones

--- src/domain/implementations/database.rs
+++ src/domain/implementations/database.rs
@@ -1,10 +1,9 @@
-use crate::domain::entities::key_value_item::KeyValueItem;
+use crate::domain::entities::key_value_item::{KeyValueItem, ValueType};
 use crate::domain::entities::key_value_item_serialized::KeyValueItemSerialized;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
-
 
 #[derive(Debug)]
 pub struct Database {
@@ -81,100 +80,8 @@ mod tests {
             items: vec![],
         };
 
-        assert_eq!(db._get_size(), 0);
-    }
-    #[test]
-    fn size_in_memory_is_correct() {
-        let kv_item = KeyValueItem::_new(
-            String::from("123"),
-            ValueType::StringType(String::from("222")),
-        );
-        let kv_item2 = KeyValueItem::_new(
-            String::from("123"),
-            ValueType::StringType(String::from("222")),
-        );
-
-        let db = Database {
-            dbfilename: "file".to_string(),
-            items: vec![kv_item, kv_item2],
-        };
-
-        assert_eq!(db._get_size(), 2);
-    }
-    #[test]
-    fn add_item() {
-        let added_item = KeyValueItem::_new(
-            String::from("nueva_key"),
-            ValueType::StringType(String::from("222")),
-        );
-        let mut db = Database {
-            dbfilename: "file".to_string(),
-            items: vec![],
-        };
-        db.add(added_item);
-
-        assert_eq!(db.items.first().unwrap().key, String::from("nueva_key"));
-        assert_eq!(
-            db.items.first().unwrap().value.to_string(),
-            String::from("222")
-        );
-        assert_eq!(db.items.len(), 1)
-    }
-    #[test]
-    fn delete_item() {
-        let added_item = KeyValueItem::_new(
-            String::from("nueva_key"),
-            ValueType::StringType(String::from("222")),
-        );
-        let mut db = Database {
-            dbfilename: "file".to_string(),
-            items: vec![added_item],
-        };
-        assert_eq!(db.items.len(), 1);
-        db._delete_by_index(0);
-        assert_eq!(db.items.len(), 0);
-    }
-    #[test]
-    fn filename_is_correct() {
-        let db = Database {
-            dbfilename: "file".to_string(),
-            items: vec![],
-        };
-        assert_eq!(db._get_filename(), "file".to_string());
-    }
-    // #[test]
-    /* fn load_items_from_file() {
-        //TODO me falta mockear el archivo para que pueda correr el test.
-        let db = Database::new("file".to_string());
-        assert_eq!(db.items.len(), 3);
-        assert_eq!(
-            db.items.get(0).unwrap().value.to_string(),
-            ValueType::StringType(String::from("222")).to_string()
-        );
-    }*/
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::domain::entities::key_value_item::ValueType;
-
-    #[test]
-    fn empty_database_returns_cero() {
-        let db = Database {
-            dbfilename: "file".to_string(),
-            items: vec![],
-        };
-
         assert_eq!(db.get_size(), 0);
     }
-
-    #[test]
-    fn database_with_two_elements_returns_2() {
-        let db = Database::new("filename".to_string());
-        assert_eq!(db.get_size(), 2);
-    }
-
     #[test]
     fn size_in_memory_is_correct() {
         let kv_item = KeyValueItem::new(
@@ -212,7 +119,6 @@ mod tests {
         );
         assert_eq!(db.items.len(), 1)
     }
-
     #[test]
     fn delete_item() {
         let added_item = KeyValueItem::new(
@@ -224,16 +130,97 @@ mod tests {
             items: vec![added_item],
         };
         assert_eq!(db.items.len(), 1);
-        db.delete_by_index(0);
+        db._delete_by_index(0);
         assert_eq!(db.items.len(), 0);
     }
-
     #[test]
     fn filename_is_correct() {
         let db = Database {
             dbfilename: "file".to_string(),
             items: vec![],
         };
-        assert_eq!(db.get_filename(), "file".to_string());
+        assert_eq!(db._get_filename(), "file".to_string());
     }
+    // #[test]
+    /* fn load_items_from_file() {
+        //TODO me falta mockear el archivo para que pueda correr el test.
+        let db = Database::new("file".to_string());
+        assert_eq!(db.items.len(), 3);
+        assert_eq!(
+            db.items.get(0).unwrap().value.to_string(),
+            ValueType::StringType(String::from("222")).to_string()
+        );
+    }*/
+}
+
+#[test]
+fn empty_database_returns_cero() {
+    let db = Database {
+        dbfilename: "file".to_string(),
+        items: vec![],
+    };
+
+    assert_eq!(db.get_size(), 0);
+}
+
+#[test]
+fn size_in_memory_is_correct() {
+    let kv_item = KeyValueItem::new(
+        String::from("123"),
+        ValueType::StringType(String::from("222")),
+    );
+    let kv_item2 = KeyValueItem::new(
+        String::from("123"),
+        ValueType::StringType(String::from("222")),
+    );
+
+    let db = Database {
+        dbfilename: "file".to_string(),
+        items: vec![kv_item, kv_item2],
+    };
+
+    assert_eq!(db.get_size(), 2);
+}
+#[test]
+fn add_item() {
+    let added_item = KeyValueItem::new(
+        String::from("nueva_key"),
+        ValueType::StringType(String::from("222")),
+    );
+    let mut db = Database {
+        dbfilename: "file".to_string(),
+        items: vec![],
+    };
+    db.add(added_item);
+
+    assert_eq!(db.items.first().unwrap().key, String::from("nueva_key"));
+    assert_eq!(
+        db.items.first().unwrap().value.to_string(),
+        String::from("222")
+    );
+    assert_eq!(db.items.len(), 1)
+}
+
+#[test]
+fn delete_item() {
+    let added_item = KeyValueItem::new(
+        String::from("nueva_key"),
+        ValueType::StringType(String::from("222")),
+    );
+    let mut db = Database {
+        dbfilename: "file".to_string(),
+        items: vec![added_item],
+    };
+    assert_eq!(db.items.len(), 1);
+    db._delete_by_index(0);
+    assert_eq!(db.items.len(), 0);
+}
+
+#[test]
+fn filename_is_correct() {
+    let db = Database {
+        dbfilename: "file".to_string(),
+        items: vec![],
+    };
+    assert_eq!(db._get_filename(), "file".to_string());
 }
